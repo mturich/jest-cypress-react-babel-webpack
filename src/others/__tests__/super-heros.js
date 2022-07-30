f13e64e5f11bc88d66f639b8a6e2fdd40b1627ca
Michael Urich <urich.michael@outlook.com>
2022-07-30 07:53:54 +0200

    inline snapshot

--- src/others/__tests__/super-heros.js
+++ src/others/__tests__/super-heros.js
@@ -3,5 +3,29 @@ import {getFlyingSuperHeros} from '../super-heros'
 test('returns super heros that can fly', () => {
   const flyingHeros = getFlyingSuperHeros()
   //console.log(flyingHeros)
-  expect(flyingHeros).toMatchSnapshot()
+  expect(flyingHeros).toMatchInlineSnapshot(`
+    Array [
+      Object {
+        "name": "Dynaguy",
+        "powers": Array [
+          "disintegration ray",
+          "fly",
+        ],
+      },
+      Object {
+        "name": "Apogee",
+        "powers": Array [
+          "gravity control",
+          "fly",
+        ],
+      },
+      Object {
+        "name": "Jack-Jack",
+        "powers": Array [
+          "shapeshifting",
+          "fly",
+        ],
+      },
+    ]
+  `)
 })
