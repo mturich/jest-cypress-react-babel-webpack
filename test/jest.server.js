6da35aa988fc1d0ad0a7fed34d26bdaf61d972e3
Michael Urich <urich.michael@outlook.com>
2022-08-06 12:40:09 +0200

    fix: tests are running for server and client now as expected

--- test/jest.server.js
+++
@@ -1,8 +0,0 @@
-const path = require('path')
-
-module.exports = {
-  ...require('./jest.common'),
-  coverageDirectory: path.join(__dirname, '../coverage/server'),
-  testEnvironment: 'jest-environment-node',
-  testMatch: ['**/__server_tests__/**/*.js'],
-}
