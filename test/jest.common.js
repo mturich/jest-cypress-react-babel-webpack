85baec1a88fb06666fb17bc841cdbc642dc50780
Michael Urich <urich.michael@outlook.com>
2022-08-06 09:34:02 +0200

    testing with multiple files works

--- test/jest.common.js
+++ test/jest.common.js
@@ -2,19 +2,11 @@ const path = require('path')
 
 module.exports = {
   rootDir: path.join(__dirname, '..'),
-  roots: [
-    '<rootDir>',
-    '/Users/michaelurich/webDev/kcd/jest-cypress-react-babel-webpack/',
-  ],
   moduleDirectories: [
     'node_modules',
     path.join(__dirname, '../src'),
     'shared',
     path.join(__dirname),
-  ],
-  modulePaths: [
-    '/Users/michaelurich/webDev/kcd/jest-cypress-react-babel-webpack/test',
-    '/Users/michaelurich/webDev/kcd/jest-cypress-react-babel-webpack/src',
   ],
   moduleNameMapper: {
     '\\.module\\.css$': 'identity-obj-proxy',
